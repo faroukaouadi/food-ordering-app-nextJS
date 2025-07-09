53b038e7d28105994706596cd86b8b6363a245d4
farouk <farouk.aouadi1@gmail.com>
2025-07-09 03:22:19 +0200

    Next Auth 2

--- src/app/[locale]/profile/page.tsx
+++ src/app/[locale]/profile/page.tsx
@@ -1,6 +1,16 @@
+import { Pages, Routes } from '@/constants/enums';
+import { Locale } from '@/i18n.config'
+import { authOptions } from '@/server/auth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-function ProfilePage() {
+async function ProfilePage({params}:{params:Promise<{locale:Locale}>}) {
+  const locale = (await params).locale;
+  const session = await getServerSession(authOptions);
+  if(!session){
+    redirect(`/${locale}/${Routes.AUTH}/${Pages.LOGIN}`);
+  }
   return (
     <main>
             <p>user</p>
